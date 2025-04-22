30007da13146eebd2a7806e561a7760e90ac2654
mahla-mohamadi <mahlamhmd78@gmail.com>
2025-04-22 16:34:43 +0330

    manzoor?

--- tests/Pest.php
+++ tests/Pest.php
@@ -10,6 +10,7 @@
 | need to change it using the "pest()" function to bind a different classes or traits.
 |
 */
+manzoret chi bood
 
 pest()->extend(Tests\TestCase::class)
  // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
