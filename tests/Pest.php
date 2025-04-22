f99f73029b835ac609c530c3c6a3c729e146650d
mahla-mohamadi <mahlamhmd78@gmail.com>
2025-04-22 16:43:03 +0330

    test-2

--- tests/Pest.php
+++ tests/Pest.php
@@ -10,7 +10,6 @@
 | need to change it using the "pest()" function to bind a different classes or traits.
 |
 */
-sd
 
 pest()->extend(Tests\TestCase::class)
  // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
